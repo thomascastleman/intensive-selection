c8ec98d826dcdee7aa3636c9dce7abf504944469
Thomas Castleman <thomas7rex@gmail.com>
2017-11-15 20:12:35 -0500

    Added buildInitialMatching

--- two-opt.py
+++ two-opt.py
@@ -1,6 +1,11 @@
 
 from main import *
 
+# construct an initial randomized but legal matching
+def buildInitialMatching(studentList, offeringList):
+	pass
+
+# returns list of tuple pairs of (student, offering)
 def two_opt(studentList, offeringList):
 	pass
 
