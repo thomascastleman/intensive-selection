dfd2cf22a1f60d3fc365e3405aa6ab58a57b8030
Thomas Castleman <thomas7rex@gmail.com>
2017-11-21 16:11:10 -0500

    Outlined matching class

---
+++ matching.py
@@ -0,0 +1,15 @@
+
+from main import *
+
+# class to handle a given matching of students to offerings; a given solution
+class Matching:
+
+	def __init__(self):
+		self.pairs = []
+		self.cost = None
+
+	# calculate the cost of an entire matching (sum of the cost of each pair)
+	def calcMatchingCost(self):
+		self.cost = 0
+		for pair in self.pairs:
+			self.cost += cost(pair)
--- student.py
+++ student.py
@@ -2,11 +2,12 @@
 
 class Student:
 
-	def __init__(self, _rank, _age, _grade, _isGhost=False):
+	def __init__(self, _id, _rank, _age, _grade, _isGhost=False):
 
 		self.isGhost = _isGhost
 
 		if not self.isGhost:
+			self.id = _id												# ID
 			self.rank = _rank											# list of top offerings in prioritized order
 			self.age = _age												# age of student
 			self.grade = _grade											# grade of student (9-12)
