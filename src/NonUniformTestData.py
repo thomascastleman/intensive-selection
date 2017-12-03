b91d3b7689f758757e2320cb7595618bfba885e4
Thomas Castleman <thomas7rex@gmail.com>
2017-12-02 20:28:19 -0500

    Updated student portion of test data

--- src/NonUniformTestData.py
+++ src/NonUniformTestData.py
@@ -12,6 +12,12 @@ Offerings:
 
 """
 
+from classes.Student import Student
+from classes.Offering import Offering
+from main import rankSize, 
+
+from random import randint
+
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import matplotlib.cm as cm
 import numpy as np
@@ -20,34 +26,51 @@ import numpy as np
 # returns tuple of ([student objects], [offering objects])
 def generateTestData(numStudents, numOfferings):
 
+	allGrades = [9, 10, 11, 12]		# grade levels
+	normalAges = [15, 16, 17, 18]	# normal age for each grade level
+	students, offerings = [], []	# arrays to hold objects
 	
+	# NEED TO GENERATE THESE POPULARITIES (SUM TO 1, length: numOfferings) ??? 
+	popularities = [] 				# 0-1 that sum to 1
+
+	# create as many students as requested
+	for stuID in range(numStudents):
+		grade = allGrades[randint(len(allGrades) - 1)]	# choose random grade
+		age = normalAges[allGrades.index(grade)] + randint(-1, 1)	# age correlates, plus or minus 1
+
+		# generate non-uniformly distributed rank
+		rank = np.random.choice(numOfferings, rankSize, replace=False, p=popularities)
+
+		s = Student(stuID, age, grade, rank)	# construct student
+		idToStudents[stuID] = s					# add to global ID hashmap
+		students.append(s)						# add to students array
 
 
-	# --------------------------------------
+	# ------------------------------------------------------
 
-	studentData = []
-	popularity = [0.21, 0.2, 0.11, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.04, 0.02,]
+	# studentData = []
+	# popularity = [0.21, 0.2, 0.11, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.04, 0.02,]
 
-	for student in range(308):
-	 studentData.append(np.random.choice(12, 5, replace=False, p=popularity))
-
-
-	totals = [0 for i in range(12)]
-	for student in range(308):
-		for rank in studentData[student]:
-			totals[rank]+=1
-		print studentData[student]
-	print "totals: ", totals 
+	# for student in range(308):
+	#  studentData.append(np.random.choice(12, 5, replace=False, p=popularity))
 
 
-	y_pos = np.arange(12)
+	# totals = [0 for i in range(12)]
+	# for student in range(308):
+	# 	for rank in studentData[student]:
+	# 		totals[rank]+=1
+	# 	print studentData[student]
+	# print "totals: ", totals 
+
+
+	# y_pos = np.arange(12)
 
 	 
-	plt.bar(y_pos, totals, align='center', alpha=0.5)
-	plt.xticks(y_pos, y_pos)
-	plt.ylabel('Frequency')
-	plt.xlabel('Intensive Choice')
+	# plt.bar(y_pos, totals, align='center', alpha=0.5)
+	# plt.xticks(y_pos, y_pos)
+	# plt.ylabel('Frequency')
+	# plt.xlabel('Intensive Choice')
 
-	plt.title('Choice Frequency')
+	# plt.title('Choice Frequency')
 	 
-	plt.show()
+	# plt.show()
