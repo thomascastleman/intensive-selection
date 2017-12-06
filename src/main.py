506f3c9f84ef7c3fdde167ee99382c860f1e8f7e
Thomas Castleman <thomas7rex@gmail.com>
2017-12-06 17:41:50 -0500

    Update todo:

--- src/main.py
+++ src/main.py
@@ -5,25 +5,35 @@
 RANKSIZE = 5
 
 # Priority coefficients
-RPCOEFF = 0.2	# rank priority
-GPCOEFF = 1.0	# grade priority
+RPCOEFF = 1.0	# rank priority
+GPCOEFF = 0.5	# grade priority
 APCOEFF = 0.0	# age priority
 
 
 """ --------------------------------------------------- """
 
-import time
+import time	# for runtime
 
 def main():
-	
+
 	import data.NonUniformTestData as td
-	import solutions.initialCSP as csp
-	import solutions.two_opt as tpt
 	import evaluation as ev
-	import util
 
 	# construct all objects, and hashmaps
 	students, offerings, idToStudents, idToOfferings = td.generateTestData(400, 20)
+
+	# use 2-opt technique
+	students = runTwoOpt(students, offerings, idToStudents, idToOfferings)
+
+	# evaluate final solution
+	print "\nRunning Evaluation..."
+	ev.evaluate(students, offerings, idToStudents, idToOfferings, False)
+
+# run a full 2-opt solution on a set of student and offering objects
+def runTwoOpt(students, offerings, idToStudents, idToOfferings):
+
+	import solutions.initialCSP as csp
+	import solutions.two_opt as tpt
 
 	# build solution
 	print "Building initial randomized matching... ",
@@ -40,10 +50,7 @@ def main():
 	students = tpt.removeAllGhostStudents(students, idToOfferings)
 	print "\nComplete in {:.3f} seconds".format(time.time() - start)
 
-	# evaluate final solution
-	print "\nRunning Evaluation..."
-	ev.evaluate(students, offerings, idToStudents, idToOfferings, True)
-
+	return students
 
 if __name__ == "__main__":
 	main()
